f1e3e4e6ff5490cfce423a258ba6ae5d48115a22
Sebastien Chaumat <sebastien.chaumat@linefactory.net>
2016-02-22 14:34:02 +0100

    More explicit message after install

--- debian-install.sh
+++ debian-install.sh
@@ -2,5 +2,5 @@
 install -oroot -groot -m644 sysconfig.btrfsmaintenance /etc/default/btrfsmaintenance
 install -d -oroot -groot -m755 /usr/share/btrfsmaintenance
 install -oroot -groot -m755 btrfs-*.sh /usr/share/btrfsmaintenance/
-echo "Now edit cron periods in /etc/default/btrfsmaintenance then run ./btrfsmaintenance-refresh-cron.sh to update cron symlinks
+echo "Now edit cron periods and mount points in /etc/default/btrfsmaintenance then run ./btrfsmaintenance-refresh-cron.sh to update cron symlinks
 "      
