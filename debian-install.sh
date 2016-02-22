74dbe38b4c7b5d8239f9aa3183b52e63a29c1943
Sebastien Chaumat <sebastien.chaumat@linefactory.net>
2016-02-22 14:12:13 +0100

    Simple installer for debian (until this is properly packaged)

---
+++ debian-install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+install -oroot -groot -m644 sysconfig.btrfsmaintenance /etc/default/btrfsmaintenance
+install -d -oroot -groot -m755 /usr/share/btrfsmaintenance
+install -oroot -groot -m755 btrfs-*.sh /usr/share/btrfsmaintenance/
+echo "Now edit cron periods in /etc/default/btrfsmaintenance then run ./btrfsmaintenance-refresh-cron.sh to update cron symlinks
+"      
