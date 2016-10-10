ecab9d3c3189ee984ca0660b67a53fbc062b60bb
David Sterba <dsterba@suse.com>
2016-10-10 13:33:38 +0200

    Make installer script generic
    There was a request to add an installer script for centos (#20), that
    only differed in the installation path. The debian-install.sh script is
    now dist-install.sh and should be fed with the right sysconfig path.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

---
+++ dist-install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# usage: $0 [sysconfdir]
+#
+# Install configuration template, documentation and scripts. Target path is
+# autodetected or can be overriden by the first argument.
+#
+# Common values of sysconfdir:
+# - /etc/sysconfig
+# - /etc/default
+
+sysconfdir="$1"
+
+if [ -z "$1" ]; then
+	if [ -d /etc/sysconfig ]; then
+		prefix=/etc/sysconfig
+	elif [ -d /etc/default ]; then
+		prefix=/etc/default
+	else
+		echo "Cannot detect sysconfig directory, please specify manually"
+		exit 1
+	fi
+else
+	prefix="$1"
+fi
+
+install -oroot -groot -m644 sysconfig.btrfsmaintenance "$sysconfdir"/btrfsmaintenance
+install -d -oroot -groot -m755 /usr/share/btrfsmaintenance
+install -oroot -groot -m755 btrfs-*.sh /usr/share/btrfsmaintenance/
+
+echo "Installation path: $sysconfdir"
+echo "Now edit cron periods and mount points in $sysconfdir/btrfsmaintenance then run ./btrfsmaintenance-refresh-cron.sh to update cron symlinks
+"
