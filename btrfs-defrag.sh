f5dc255715a0bda528bb75957e0c65bc73732945
David Sterba <dsterba@suse.com>
2025-01-23 18:59:43 +0100

    btrfs-defrag: respect setting of BTRFS_ALLOW_CONCURRENCY
    As reported, the defrag task does not run with the concurrency
    protection so it can cause huge IO spikes when there's some other task
    running as well.
    
    Issue: #115
    Signed-off-by: David Sterba <dsterba@suse.com>

--- btrfs-defrag.sh
+++ btrfs-defrag.sh
@@ -27,7 +27,7 @@ for P in $BTRFS_DEFRAG_PATHS; do
 		echo "Path $P is not btrfs, skipping"
 		continue
 	fi
-	find "$P" -xdev -size "$BTRFS_DEFRAG_MIN_SIZE" -type f \
+	run_task find "$P" -xdev -size "$BTRFS_DEFRAG_MIN_SIZE" -type f \
 		-exec btrfs filesystem defrag -t 32m -f $BTRFS_VERBOSITY '{}' \;
 done
 
